9816e96a54d6be0655bcbae6c2964f3262f730ed
jwu03 <jwu03@stuy.edu>
2019-03-31 23:39:56 -0400

    added node class

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,34 @@
 class MyLinkedList <E> {
-  class Node <E>{
-    E data;
-  }
+  class Node{
+ private E data;
+ private Node next,prev;
+ public Node(Node prev2, E number, Node next2){
+   prev = prev2;
+   data = number;
+   next = next2;
+ }
+ public Node next(){
+   return next;
+ }
+ public Node prev(){
+   return prev;
+ }
+ public void setNext(Node other){
+   next = other;
+ }
+ public void setPrev(Node other){
+   prev = other;
+ }
+ public E getData(){
+   return data;
+ }
+ public E setData(E i){
+   data = i;
+   return data;
+ }
+ public String toString(){
+   return data + "";
+ }
+}
+
 }
