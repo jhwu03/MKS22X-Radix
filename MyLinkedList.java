e452a3d597c7a88837822ee9a7e0dab20bcf741b
jwu03 <jwu03@stuy.edu>
2019-03-31 23:49:28 -0400

    added add method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,7 +2,7 @@ class MyLinkedList <E> {
   class Node{
  private E data;
  private Node next,prev;
- public Node(Node prev2, E number, Node next2){
+ public Node(E number, Node prev2, Node next2){
    prev = prev2;
    data = number;
    next = next2;
@@ -30,5 +30,50 @@ class MyLinkedList <E> {
    return data + "";
  }
 }
+
+private int length;
+  private Node start,end;
+
+  public MyLinkedList(){}
+
+  public int size(){
+    return length;
+  }
+
+  public String toString(){
+    String ans = "[";
+    Node temp = start;
+    while (temp != null){
+      ans += temp;
+      temp = temp.next();
+      if (temp != null) ans += ", ";
+    }
+    ans += "]";
+    return ans;
+  }
+
+  public void clear(){
+    length = 0;
+    start = null;
+    end = null;
+  }
+
+  public boolean add(E value){
+   if (start == null){
+     start = new Node(value, null, null);
+     end = start;
+   }
+   else if (end == start){
+     end = new Node(value, null, start);
+     start.setNext(end);
+   }
+   else{
+     Node temp = new Node(value, null, end);
+     end.setNext(temp);
+     end = temp;
+   }
+   length++;
+   return true;
+ }
 
 }
